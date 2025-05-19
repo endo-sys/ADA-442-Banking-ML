6c634a26b1a0c1031fc0100fd6eae27bb074b03f
eneserdogan <enes70442@gmail.com>
2025-05-19 21:42:51 +0300

    pipeline added

--- pipeline/train.py
+++ pipeline/train.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split, GridSearchCV, StratifiedKFold
 from sklearn.preprocessing import RobustScaler, OneHotEncoder
 from sklearn.impute import SimpleImputer
